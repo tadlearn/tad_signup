198c83208f2e621d80b44d499313d197fcc29731
tadlearn <tad.learn@gmail.com>
2021-07-11 09:28:04 +0800

    新增 TadDataCenter 用的資料表

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -21,3 +21,17 @@ CREATE TABLE `tad_signup_data` (
   `accept` enum('1','0') DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `tad_signup_data_center` (
+  `mid` mediumint(9) unsigned NOT NULL AUTO_INCREMENT COMMENT '模組編號',
+  `col_name` varchar(100) NOT NULL DEFAULT '' COMMENT '欄位名稱',
+  `col_sn` mediumint(9) unsigned NOT NULL DEFAULT '0' COMMENT '欄位編號',
+  `data_name` varchar(100) NOT NULL DEFAULT '' COMMENT '資料名稱',
+  `data_value` text NOT NULL COMMENT '儲存值',
+  `data_sort` mediumint(9) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  `col_id` varchar(100) NOT NULL COMMENT '辨識字串',
+  `sort` mediumint(9) unsigned COMMENT '顯示順序',
+  `update_time` datetime NOT NULL COMMENT '更新時間',
+  PRIMARY KEY (`mid`,`col_name`,`col_sn`,`data_name`,`data_sort`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- xoops_version.php
+++ xoops_version.php
@@ -36,7 +36,7 @@ $modversion['system_menu'] = 1;
 
 //---模組資料表架構---//
 $modversion['sqlfile']['mysql'] = 'sql/mysql.sql';
-$modversion['tables'] = ['tad_signup_actions', 'tad_signup_data'];
+$modversion['tables'] = ['tad_signup_actions', 'tad_signup_data', 'tad_signup_data_center'];
 
 //---後台管理介面設定---//
 $modversion['hasAdmin'] = 1;
