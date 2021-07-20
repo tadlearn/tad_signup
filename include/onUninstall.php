80bd7befd9f85357e44da661c152110727dd7737
tadlearn <tad.learn@gmail.com>
2021-07-20 12:04:43 +0800

    移除模組時的自動功能

--- include/onUninstall.php
+++ include/onUninstall.php
@@ -1,13 +1,15 @@
 <?php
-// 如「模組目錄」= signup，則「首字大寫模組目錄」= Signup
-// 如「資料表名」= actions，則「模組物件」= Actions
+// 反安裝前
+function xoops_module_pre_uninstall_tad_signup(XoopsModule $module)
+{
+}
 
-function xoops_module_uninstall_模組目錄($module)
+// 反安裝後
+function xoops_module_uninstall_tad_signup(XoopsModule $module)
 {
     global $xoopsDB;
     $date = date("Ymd");
-
-    rename(XOOPS_ROOT_PATH . "/uploads/模組目錄", XOOPS_ROOT_PATH . "/uploads/模組目錄_bak_{$date}");
+    rename(XOOPS_ROOT_PATH . "/uploads/tad_signup", XOOPS_ROOT_PATH . "/uploads/tad_signup_bak_{$date}");
 
     return true;
 }
