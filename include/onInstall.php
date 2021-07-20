2287532f287af1b260e7a63a0634a37f2442459a
tadlearn <tad.learn@gmail.com>
2021-07-20 11:39:29 +0800

    安裝模組時的自動功能

--- include/onInstall.php
+++ include/onInstall.php
@@ -1,20 +1,22 @@
 <?php
-// 如「模組目錄」= signup，則「首字大寫模組目錄」= Signup
-// 如「資料表名」= actions，則「模組物件」= Actions
-
 use XoopsModules\Tadtools\Utility;
 if (!class_exists('XoopsModules\Tadtools\Utility')) {
     require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
 }
 
-function xoops_module_install_模組目錄(&$module)
+// 安裝前
+function xoops_module_pre_install_tad_signup(XoopsModule $module)
+{
+}
+
+// 安裝後
+function xoops_module_install_tad_signup(XoopsModule $module)
 {
     // 有上傳功能才需要
-    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/模組目錄");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_signup");
     // 若有用到CKEditor編輯器才需要
-    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/模組目錄/file");
-    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/模組目錄/image");
-    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/模組目錄/image/.thumbs");
-
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_signup/file");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_signup/image");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_signup/image/.thumbs");
     return true;
 }
--- xoops_version.php
+++ xoops_version.php
@@ -48,8 +48,8 @@ $modversion['hasMain'] = 1;
 // $modversion['sub'][] = ['name' => '子選項文字', 'url' => '子選項連結位址'];
 
 //---模組自動功能---//
-// $modversion['onInstall'] = "include/install.php";
-// $modversion['onUpdate'] = "include/update.php";
+$modversion['onInstall'] = "include/onInstall.php";
+// $modversion['onUpdate'] = "include/onUpdate.php";
 // $modversion['onUninstall'] = "include/onUninstall.php";
 
 //---樣板設定---//
