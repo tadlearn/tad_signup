d3dd677879b5b8e09a4f57e1dbd7d19a42ecf534
tadlearn <tad.learn@gmail.com>
2021-07-24 15:00:14 +0800

    產生 Excel 匯入檔

--- excel.php
+++ excel.php
@@ -1,13 +1,49 @@
 <?php
-use XoopsModules\Tadtools\Utility;
+use Xmf\Request;
+use XoopsModules\Tad_signup\Tad_signup_actions;
+use XoopsModules\Tad_signup\Tad_signup_data;
 /*-----------引入檔案區--------------*/
 require_once __DIR__ . '/header.php';
+
+if (!$_SESSION['can_add']) {
+    redirect_header($_SERVER['PHP_SELF'], 3, "您沒有權限使用此功能");
+}
+
+$id = Request::getInt('id');
+$type = Request::getString('type');
+
+$action = Tad_signup_actions::get($id);
+if ($action['uid'] != $xoopsUser->uid()) {
+    redirect_header($_SERVER['PHP_SELF'], 3, "您沒有權限使用此功能");
+}
 
 require_once XOOPS_ROOT_PATH . '/modules/tadtools/vendor/phpoffice/phpexcel/Classes/PHPExcel.php'; //引入 PHPExcel 物件庫
 require_once XOOPS_ROOT_PATH . '/modules/tadtools/vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php'; //引入PHPExcel_IOFactory 物件庫
 $objPHPExcel = new PHPExcel(); //實體化Excel
 //----------內容-----------//
-$title = 'test';
+$title = "{$action['title']}報名名單";
+$objPHPExcel->setActiveSheetIndex(0); //設定預設顯示的工作表
+$objActSheet = $objPHPExcel->getActiveSheet(); //指定預設工作表為 $objActSheet
+$objActSheet->setTitle($title); //設定標題
+$objPHPExcel->createSheet(); //建立新的工作表，上面那三行再來一次，編號要改
+
+// 抓出標題資料
+$head_row = explode("\n", $action['setup']);
+$head = [];
+foreach ($head_row as $head_data) {
+    $cols = explode(',', $head_data);
+    if (strpos($cols[0], '#') === false) {
+        $head[] = str_replace('*', '', trim($cols[0]));
+    }
+}
+$head[] = '錄取';
+$head[] = '報名日期';
+$head[] = '身份';
+
+foreach ($head as $column => $value) {
+    $objActSheet->setCellValueByColumnAndRow($column, 1, $value); //直欄從0開始，橫列從1開始
+}
+
 // $title = (_CHARSET === 'UTF-8') ? iconv('UTF-8', 'Big5', $title) : $title;
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header("Content-Disposition: attachment;filename={$title}.xlsx");
