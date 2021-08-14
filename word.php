881e8e967ca202e1cd578943b3e3b2402acfc77f
tadlearn <tad.learn@gmail.com>
2021-08-14 17:54:00 +0800

    將值套用到樣板的表格中

--- word.php
+++ word.php
@@ -1,6 +1,7 @@
 <?php
 use Xmf\Request;
 use XoopsModules\Tad_signup\Tad_signup_actions;
+use XoopsModules\Tad_signup\Tad_signup_data;
 use \PhpOffice\PhpWord\TemplateProcessor;
 /*-----------引入檔案區--------------*/
 require_once __DIR__ . '/header.php';
@@ -22,7 +23,31 @@ $templateProcessor->setValue('number', $action['number']);
 $templateProcessor->setValue('candidate', $action['candidate']);
 $templateProcessor->setValue('signup', count($action['signup']));
 $templateProcessor->setValue('url', XOOPS_URL . "/modules/tad_signup/index.php?op=tad_signup_data_create&amp;action_id={$action['id']}");
-// $templateProcessor->saveAs("{$action['title']}報名名單.docx");
+
+$signup = Tad_signup_data::get_all($action['id']);
+$templateProcessor->cloneRow('id', count($signup));
+
+$i = 1;
+foreach ($signup as $id => $signup_data) {
+    $iteam = [];
+    foreach ($signup_data['tdc'] as $head => $user_data) {
+        $iteam[] = $head . '：' . implode('、', $user_data);
+    }
+    $data = implode('<w:br/>', $iteam);
+
+    if ($signup_data['accept'] === '1') {
+        $accept = '錄取';
+    } elseif ($signup_data['accept'] === '0') {
+        $accept = '未錄取';
+    } else {
+        $accept = '尚未設定';
+    }
+
+    $templateProcessor->setValue("id#{$i}", $id);
+    $templateProcessor->setValue("accept#{$i}", $accept);
+    $templateProcessor->setValue("data#{$i}", $data);
+    $i++;
+}
 
 header('Content-Type: application/vnd.ms-word');
 header("Content-Disposition: attachment;filename={$action['title']}報名名單.docx");
