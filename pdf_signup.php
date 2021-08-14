d5ba2d733007205dcafe76f1b03beb991b080b24
tadlearn <tad.learn@gmail.com>
2021-08-14 15:40:47 +0800

    產出套用設定的PDF檔

--- pdf_signup.php
+++ pdf_signup.php
@@ -1,5 +1,6 @@
 <?php
 use Xmf\Request;
+use XoopsModules\Tadtools\TadDataCenter;
 use XoopsModules\Tad_signup\Tad_signup_actions;
 use XoopsModules\Tad_signup\Tad_signup_data;
 /*-----------引入檔案區--------------*/
@@ -30,25 +31,36 @@ $pdf->SetFont('droidsansfallback', '', 16, '', true); //設定字型
 $pdf->Cell(40, 20, '活動日期：', 0, 0);
 $pdf->Cell(150, 20, $action['action_date'], 0, 1);
 
-// $pdf->MultiCell( $w, $h, $txt, $border = 0, $align = 'J', $fill = false, $ln = 1, $x = '', $y = '', $reseth = true, $stretch = 0, $ishtml = false, $autopadding = true, $maxh = 0, $valign = 'T', $fitcell = false );
-// $pdf->Cell($w, $h = 0, $txt = '', $border = 0, $ln = 0, $align = '', $fill = 0, $link = nil, $stretch = 0, $ignore_min_height = false, $calign = 'T', $valign = 'M')
+$TadDataCenter = new TadDataCenter('tad_signup');
+$TadDataCenter->set_col('pdf_setup_id', $id);
+$pdf_setup_col = $TadDataCenter->getData('pdf_setup_col', 0);
+$col_arr = explode(',', $pdf_setup_col);
+
+$col_count = count($col_arr);
+if (empty($col_count)) {
+    $col_count = 1;
+}
 
 $h = 15;
+$w = 120 / $col_count;
 $maxh = 15;
 $pdf->Cell(15, $h, '編號', 1, 0, 'C');
-$pdf->Cell(40, $h, '姓名', 1, 0, 'C');
-$pdf->Cell(35, $h, '飲食', 1, 0, 'C');
-$pdf->Cell(100, $h, '簽名', 1, 1, 'C');
+foreach ($col_arr as $col_name) {
+    $pdf->Cell($w, $h, $col_name, 1, 0, 'C');
+}
+$pdf->Cell(55, $h, '簽名', 1, 1, 'C');
 
 $signup = Tad_signup_data::get_all($action['id'], null, true, true);
 // Utility::dd($signup);
 $i = 1;
 foreach ($signup as $signup_data) {
     $pdf->MultiCell(15, $h, $i, 1, 'C', false, 0, '', '', true, 0, false, true, $maxh, 'M');
-    $pdf->MultiCell(40, $h, implode('、', $signup_data['tdc']['姓名']), 1, 'C', false, 0, '', '', true, 0, false, true, $maxh, 'M');
-    $pdf->MultiCell(35, $h, implode('、', $signup_data['tdc']['飲食']), 1, 'C', false, 0, '', '', true, 0, false, true, $maxh, 'M');
-    $pdf->MultiCell(100, $h, '', 1, 'C', false, 1, '', '', true, 0, false, true, $maxh, 'M');
+    foreach ($col_arr as $col_name) {
+        $pdf->MultiCell($w, $h, implode('、', $signup_data['tdc'][$col_name]), 1, 'C', false, 0, '', '', true, 0, false, true, $maxh, 'M');
+    }
+
+    $pdf->MultiCell(55, $h, '', 1, 'C', false, 1, '', '', true, 0, false, true, $maxh, 'M');
     $i++;
 }
 
-$pdf->Output("{$title}.pdf", 'I');
+$pdf->Output("{$title}.pdf", 'D');
