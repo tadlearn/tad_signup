5257b1d9e2870da77abefd7a9f35d9903be04624
tadlearn <tad.learn@gmail.com>
2021-08-14 17:00:03 +0800

    插入表格到word中

--- word_signup.php
+++ word_signup.php
@@ -3,6 +3,7 @@ use PhpOffice\PhpWord\IOFactory;
 use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\PhpWord\Shared\Converter;
 use Xmf\Request;
+use XoopsModules\Tadtools\TadDataCenter;
 use XoopsModules\Tad_signup\Tad_signup_actions;
 use XoopsModules\Tad_signup\Tad_signup_data;
 /*-----------引入檔案區--------------*/
@@ -34,6 +35,14 @@ $paraStyle = ['align' => 'center', 'valign' => 'center'];
 $left_paraStyle = ['align' => 'left', 'valign' => 'center'];
 // 靠右段落樣式設定
 $right_paraStyle = ['align' => 'right', 'valign' => 'center'];
+// 表格樣式設定
+$tableStyle = ['borderColor' => '000000', 'borderSize' => 6, 'cellMargin' => 80];
+// 橫列樣式
+$rowStyle = ['cantSplit' => true, 'tblHeader' => true];
+// 儲存格標題文字樣式設定
+$headStyle = ['bold' => true];
+// 儲存格內文段落樣式設定
+$cellStyle = ['valign' => 'center'];
 
 $phpWord->addTitleStyle(1, $Title1Style, $paraStyle); //設定標題1樣式
 $phpWord->addTitleStyle(2, $Title2Style, $paraStyle); //設定標題1樣式
@@ -50,6 +59,39 @@ $title = "{$action['title']}簽到表";
 $section->addTitle($title, 1); //新增標題
 $section->addTextBreak(1);
 $section->addText("活動日期：{$action['action_date']}", $fontStyle, $left_paraStyle);
+$section->addTextBreak(1);
+
+$TadDataCenter = new TadDataCenter('tad_signup');
+$TadDataCenter->set_col('pdf_setup_id', $id);
+$pdf_setup_col = $TadDataCenter->getData('pdf_setup_col', 0);
+$col_arr = explode(',', $pdf_setup_col);
+$col_count = count($col_arr);
+if (empty($col_count)) {
+    $col_count = 1;
+}
+
+$w = 10.6 / $col_count;
+
+$table = $section->addTable($tableStyle);
+$table->addRow();
+$table->addCell(Converter::cmToTwip(1.5), $cellStyle)->addText('編號', $fontStyle, $paraStyle);
+foreach ($col_arr as $col_name) {
+    $table->addCell(Converter::cmToTwip($w), $cellStyle)->addText($col_name, $fontStyle, $paraStyle);
+}
+$table->addCell(Converter::cmToTwip(4.5), $cellStyle)->addText('簽名', $fontStyle, $paraStyle);
+
+$signup = Tad_signup_data::get_all($action['id'], null, true, true);
+$i = 1;
+foreach ($signup as $signup_data) {
+    $table->addRow();
+    $table->addCell(Converter::cmToTwip(1.5), $cellStyle)->addText($i, $fontStyle, $paraStyle);
+    foreach ($col_arr as $col_name) {
+        $table->addCell(Converter::cmToTwip($w), $cellStyle)->addText(implode('、', $signup_data['tdc'][$col_name]), $fontStyle, $paraStyle);
+    }
+
+    $table->addCell(Converter::cmToTwip(4.5), $cellStyle)->addText('', $fontStyle, $paraStyle);
+    $i++;
+}
 
 $objWriter = IOFactory::createWriter($phpWord, 'Word2007');
 header('Content-Type: application/vnd.ms-word');
