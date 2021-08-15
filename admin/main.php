e3cb926b1d848a174606351e5fd43c701dc97fcc
tadlearn <tad.learn@gmail.com>
2021-08-15 13:05:14 +0800

    admin\main.php 套用語系

--- admin/main.php
+++ admin/main.php
@@ -25,7 +25,7 @@ switch ($op) {
     case 'tad_signup_actions_store':
         $id = Tad_signup_actions::store();
         // header("location: {$_SERVER['PHP_SELF']}?id=$id");
-        redirect_header($_SERVER['PHP_SELF'] . "?id=$id", 3, "成功建立活動！");
+        redirect_header($_SERVER['PHP_SELF'] . "?id=$id", 3, _MA_TAD_SIGNUP_CREATE_SUCCESS);
         exit;
 
     //修改用表單
@@ -38,14 +38,14 @@ switch ($op) {
     case 'tad_signup_actions_update':
         Tad_signup_actions::update($id);
         // header("location: {$_SERVER['PHP_SELF']}?id=$id");
-        redirect_header($_SERVER['PHP_SELF'] . "?id=$id", 3, "成功修改活動！");
+        redirect_header($_SERVER['PHP_SELF'] . "?id=$id", 3, _MA_TAD_SIGNUP_UPDATE_SUCCESS);
         exit;
 
     //刪除資料
     case 'tad_signup_actions_destroy':
         Tad_signup_actions::destroy($id);
         // header("location: {$_SERVER['PHP_SELF']}");
-        redirect_header($_SERVER['PHP_SELF'], 3, "成功刪除活動！");
+        redirect_header($_SERVER['PHP_SELF'], 3, _MA_TAD_SIGNUP_DESTROY_SUCCESS);
         exit;
 
     default:
--- language/tchinese_utf8/admin.php
+++ language/tchinese_utf8/admin.php
@@ -1,2 +1,6 @@
 <?php
 xoops_loadLanguage('admin_common', 'tadtools');
+
+define('_MA_TAD_SIGNUP_CREATE_SUCCESS', '成功建立活動！');
+define('_MA_TAD_SIGNUP_UPDATE_SUCCESS', '成功修改活動！');
+define('_MA_TAD_SIGNUP_DESTROY_SUCCESS', '成功刪除活動！');
