1f4a93766289d31514e287510a36d00554013acb
tadlearn <tad.learn@gmail.com>
2021-08-15 14:47:07 +0800

    admin\power.php 套用語系

--- language/tchinese_utf8/admin.php
+++ language/tchinese_utf8/admin.php
@@ -1,6 +1,12 @@
 <?php
 xoops_loadLanguage('admin_common', 'tadtools');
 
+// admin\main.php
 define('_MA_TAD_SIGNUP_CREATE_SUCCESS', '成功建立活動！');
 define('_MA_TAD_SIGNUP_UPDATE_SUCCESS', '成功修改活動！');
 define('_MA_TAD_SIGNUP_DESTROY_SUCCESS', '成功刪除活動！');
+
+// admin\power.php
+define('_MA_TAD_SIGNUP_CREATE_ACTION', '建立報名活動');
+define('_MA_TAD_SIGNUP_PERMISSION_SETTING', '細部權限設定');
+define('_MA_TAD_SIGNUP_CHECK_PERMISSIONS', '請勾選欲開放給群組使用的權限：');
