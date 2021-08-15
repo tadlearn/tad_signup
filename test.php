5b403c5ed94f47620628e9c1887f0712638037bc
tadlearn <tad.learn@gmail.com>
2021-08-15 12:32:03 +0800

    接收 API 並產生頁面

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/header.php';
+require_once XOOPS_ROOT_PATH . '/header.php';
+
+$url = "http://odata.tn.edu.tw/ebookApi/api/getOpenCourse/?year=110";
+$json = file_get_contents($url);
+$arr = json_decode($json, true);
+// var_dump($arr);
+// var_export($arr);
+
+$content = "<ol>";
+foreach ($arr as $action) {
+    $content .= "<li>{$action['courseDay']} {$action['courseName']}</li>";
+}
+$content .= "</ol>";
+
+echo $content;
+require_once XOOPS_ROOT_PATH . '/footer.php';
