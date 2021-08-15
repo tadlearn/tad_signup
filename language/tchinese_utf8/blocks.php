aa647915acc57bb2ed3f95cd18d60dbc5124a39d
tadlearn <tad.learn@gmail.com>
2021-08-15 14:57:18 +0800

    blocks\action_list.php 套用語系

--- language/tchinese_utf8/blocks.php
+++ language/tchinese_utf8/blocks.php
@@ -1,1 +1,8 @@
 <?php
+// blocks\action_list.php
+define('_MB_TAD_SIGNUP_SHOW_ACTIONS_NUMBER', '顯示活動數');
+define('_MB_TAD_SIGNUP_ORDER_BY', '排序依據');
+define('_MB_TAD_SIGNUP_ORDER_BY_ACTION_DATE', '活動日期從遠到近');
+define('_MB_TAD_SIGNUP_ORDER_BY_ACTION_DATE_DESC', '活動日期從近到遠');
+define('_MB_TAD_SIGNUP_ORDER_BY_END_DATE', '報名截止日期從遠到近');
+define('_MB_TAD_SIGNUP_ORDER_BY_END_DATE_DESC', '報名截止日期從近到遠');
